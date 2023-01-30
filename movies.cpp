d80b1a86b5b75a1934570977723e2bf89010666f
Anihilat0r <87810236+Anihilat0r@users.noreply.github.com>
2023-01-30 18:02:04 +0200

    Add files via upload

---
+++ movies.cpp
@@ -0,0 +1,195 @@
+#include "decls.h"
+#include <float.h>
+
+/*
+	This file contains the definitions for the members of the "movies" class.
+*/
+
+movie::movie() {
+	title = "";
+	score = 0;
+	cost_per_day = 0;
+	year_released = 0;
+	copies_available = 0;
+	stock = 0;
+};
+
+string movie::GetTitle() {
+	return title;
+}
+int movie::GetYear() {
+	return year_released;
+}
+float movie::GetCostperday() {
+	return cost_per_day;
+}
+float movie::GetScore() {
+	return score;
+}
+int movie::GetCopies() {
+	return copies_available;
+}
+int movie::GetStock() {
+	return stock;
+}
+
+void movie::SetTitle(string title) {
+	this->title = title;
+}
+void movie::SetYear(int year) {
+	this->year_released = year;
+}
+void movie::SetCostperday(float cost) {
+	this->cost_per_day = cost;
+}
+void movie::SetScore(float score) {
+	this->score = score;
+}
+void movie::SetCopies(int copies) {
+	this->copies_available = copies;
+}
+void movie::SetStock(int stock) {
+	this->stock = stock;
+}
+
+movie::~movie() {
+	//cout << "Movie : " << id << " deleted.\n";
+};
+
+void movie::modify_copies(std::vector<movie>& movie_list) {
+	bool found = false;
+	string temp_data;
+	int int_data;
+	float float_data;
+
+	//Get the current date in order to check movie release year validity
+	tm current_time;
+	time_t now = time(0);
+	localtime_s(&current_time, &now);
+
+	cout << "Enter movie title : ";
+	this->title = user_input_other();
+
+	cout << "Enter the year it was released (format:YYYY) : ";
+	std::getline(cin, temp_data);
+
+	int_data = stoi(temp_data);
+	//The earliest motion picture known was made in 1888
+	//Check if the lengths of the string input and its integer conversion are equal bcs 
+	//stoi() accepts inputs along the lines of "1999abcd" and extracts the integer, which is not desired
+	while (temp_data.length() != std::to_string(int_data).length() || int_data < 1888 || int_data > current_time.tm_year + 1900) {
+
+		cout << "The year entered is invalid, please enter the correct one.\n";
+		std::getline(cin, temp_data);
+		int_data = stoi(temp_data);
+	}
+	this->year_released = int_data;
+
+	for (movie& i : movie_list) {
+
+		//find movie in vector using title and date
+		if (i.title == this->title && i.year_released == this->year_released) {
+
+			modify_existing_listing(movie_list, i);
+
+			found = true;
+			break;
+		}
+	};
+
+	//If the movie soesn't exist in the catalogue enter the rest of its information
+	if (found == false) {
+
+		cout << "Enter movie's review score (between 0 - 10) : ";
+		std::getline(cin, temp_data);
+		float_data = stof(temp_data);
+
+		while (float_data <= 0 || float_data >= 10) {
+
+			cout << "The movie's score entered is invalid, please enter the correct one.\n";
+			std::getline(cin, temp_data);
+			float_data = stof(temp_data);
+		}
+		this->score = float_data;
+
+		cout << "Enter the cost of rent per day : ";
+		std::getline(cin, temp_data);
+		float_data = stof(temp_data);
+
+		while (float_data <= 0 || float_data >= 100) {
+
+			cout << "The movie's cost entered is invalid, please enter the correct one.\n";
+			std::getline(cin, temp_data);
+			float_data = stof(temp_data);
+		}
+		this->cost_per_day = float_data;
+
+		cout << "Enter the stock of copies available for this movie : ";
+		std::getline(cin, temp_data);
+		int_data = stoi(temp_data);
+
+		while (int_data <= 0 || int_data != stof(temp_data)) {
+
+			cout << "The movie's stock number is invalid, please enter the correct one.\n";
+			std::getline(cin, temp_data);
+			int_data = stof(temp_data);
+		}
+		this->stock = int_data;
+		this->copies_available = this->stock;
+
+		movie_list.push_back(*this);
+	}
+};
+
+void movie::modify_existing_listing(std::vector<movie>& movie_list, movie& current_movie) {
+	int extra_copies, movie_position;
+	string temp_data;
+
+	cout << "Enter the number to modify copies : ";
+	std::getline(cin, temp_data);
+
+	while (stoi(temp_data) != stof(temp_data)) {
+
+		cout << "The number of copies has to be an integer, please enter the correct one.\n";
+		std::getline(cin, temp_data);
+	}
+	extra_copies = stoi(temp_data);
+
+	//The following conditions work because stock >= copies_available always.
+	if (current_movie.copies_available + extra_copies < 0) {
+		cout << "Can't remove more copies than the ones we have.";
+	}
+	else if (current_movie.stock + extra_copies == 0) {
+		//Find index of object "i" in vector in order to remove it
+		movie_position = &current_movie - &movie_list[0];
+		movie_list.erase(movie_list.begin() + movie_position);
+	}
+	else {
+		//Add the extra_copies to the existing stock
+		current_movie.stock += extra_copies;
+		current_movie.copies_available += extra_copies;
+	}
+};
+
+void movie_catalogue_modification(std::vector<movie>& movie_list)
+{
+	string user_choice;
+	movie m1;
+
+	cout << "Welcome to the movie catalogue modification!\n";
+	do {
+		try {
+			m1.modify_copies(movie_list);
+		}
+		catch (const std::invalid_argument& ia) {
+			std::cerr << "The value entered is not appropriate : " << ia.what() << '\n';
+		}
+
+		cout << "Continue modifying the movie catalogue? (Y/N) : ";
+		std::getline(cin, user_choice);
+
+		if (user_choice.size() != 1) break;
+
+	} while (user_choice[0] == 'Y');
+
+};
---
+++ users.cpp
@@ -0,0 +1,176 @@
+#include "decls.h"
+
+/*
+	This file contains the definitions for the members of the "user" class and its child classes "employee" and "customer".
+*/
+
+user::user() {
+	id = 0;
+	name = "";
+	username = "";
+	password = "";
+}
+void user::SetId(int identity) {
+	this->id = identity;
+}
+void user::SetUsername(string un) {
+	this->username = un;
+}
+void user::SetName(string nm) {
+	this->name = nm;
+}
+void user::SetPassword(string ps) {
+	this->password = ps;
+}
+int user::GetId() {
+	return id;
+}
+string user::GetName() {
+	return name;
+}
+string user::GetUsername() {
+	return username;
+}
+string user::GetPassword() {
+	return password;
+}
+
+//salary 850 is a random value, currently there isn't a way implemented to update this value.
+employee::employee() {
+	salary = 850;
+}
+void employee::SetSalary(int salary) {
+	this->salary = salary;
+}
+int employee::GetSalary() {
+	return salary;
+}
+employee::~employee() {
+	//cout << "employee member : " << id << " deleted.\n";
+}
+
+customer::customer() {
+	current_movie = "<none>";
+	movie_year = 0;
+	rent_date = 0;
+	current_cost = 0;
+}
+void customer::SetCost(float cost) {
+	this->current_cost = cost;
+}
+void customer::SetRentDate(int date) {
+	this->rent_date = date;
+}
+void customer::SetCurrentMovie(string curr_movie) {
+	this->current_movie = curr_movie;
+}
+void customer::SetMovieYear(int year) {
+	this->movie_year = year;
+}
+string customer::GetCurrentMovie() {
+	return current_movie;
+}
+time_t customer::GetRentDate() {
+	return rent_date;
+}
+float customer::GetCost() {
+	return current_cost;
+}
+int customer::GetMovieYear() {
+	return movie_year;
+}
+customer::~customer() {
+	//cout << "Customer : " << id << " deleted.\n";
+}
+
+void customer::rent_movie(std::vector<movie> movie_list) {
+	
+	print_list(movie_list, "customer");
+	
+	if (movie_list.empty()) return;
+	
+	string title;
+	int year = 0;
+	bool found = false;
+
+	//The following variables exist in order to get the current year to check for user input year validity
+	tm current_time;
+	time_t now = time(0);
+	localtime_s(&current_time, &now);
+
+	cout << "Enter the title of the movie you would like to rent : ";
+	std::getline(cin, title);
+
+	cout << "Enter the year it was released (format:YYYY) : ";
+	year = stoi(user_input_log());
+
+	//The earliest motion picture known was made in 1888
+	while (year < 1888 || year > current_time.tm_year + 1900) {
+
+		cout << "The year entered is invalid, please enter the correct one.\n";
+		year = stoi(user_input_log());
+	}
+
+	for (movie& i : movie_list) {
+
+		//find movie in vector using title and date
+		if (i.GetTitle() == title && i.GetYear() == year) {
+
+			if (i.GetStock() > 0) {
+
+				i.SetStock(i.GetStock() - 1);
+				this->current_movie = i.GetTitle();
+				this->movie_year = i.GetYear();
+				this->current_cost = i.GetCostperday();
+				//save the time the movie was rented
+				this->rent_date = time(0);
+			}
+			else cout << "There are no copies of " << title << " available right now.\n";
+			found = true;
+			break;
+		}
+	};
+	if (found == false) cout << title << " is not available at our stores.\n";
+}
+
+void customer::return_movie(std::vector<movie> movie_list) {
+
+	double days, cost;
+	tm return_time, rent_time;
+	time_t now = time(0);
+	localtime_s(&return_time, &now);
+	localtime_s(&rent_time, &this->rent_date);
+
+	//Find the number of days to charge for rent
+	days = (now - this->rent_date) / 86400;
+
+	if (int(days) == 0) {
+		cout << "The minimum charge for renting " << this->GetCurrentMovie() << " is " << this->GetCost() << '\n';
+	}
+	//If return time is earlier than rent time means it is the next day of int(days)
+	else if (return_time.tm_hour < rent_time.tm_hour + int(days)) {
+		cost = int(days + 1) * this->GetCost();
+		cout << "That will be " << cost << " for the " << int(days) + 1 << " days of rent.\n";
+	}
+	else {
+		cost = int(days) * this->GetCost();
+		cout << "That will be " << int(days) << " for the " << int(days) << " days of rent.\n";
+	}
+
+	//Find the movie returned and +1 its stock
+	for (movie& i : movie_list) {
+
+		//find movie in vector using title and date
+		if (i.GetTitle() == this->current_movie && i.GetYear() == this->movie_year) {
+
+			i.SetStock(i.GetStock() + 1);
+			break;
+		}
+	};
+
+	//Update the current customer's data in the map
+	this->current_movie = "<none>";
+	this->current_cost = 0;
+	this->rent_date = 0;
+	this->movie_year = 0;
+}
